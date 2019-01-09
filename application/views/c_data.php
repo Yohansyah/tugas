008f635f83fe7c9cc3685ce72814d794a4e08cbf
Yohansyah <yohan.syahfudi@gmail.com>
2019-01-09 20:43:13 +0700

    data

--- application/views/c_data.php
+++ application/views/c_data.php
@@ -1,39 +1,22 @@
-<section id="data" name="data">
-  <div id="data">
-    <div style="background: #a9a9a9;">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-7">
-            <h3>Data Obat</h3>
-            <br>
-            <form class="contact-form php-mail-form" role="form" action="contactform/contactform.php" method="POST">
-              <div class="form-group">
-                <label for="contact-kode">Kode</label>
-                <input type="kode" name="kode" class="form-control" id="contact-kode" placeholder="Kode" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-                <div class="validate"></div>
-              </div>
-              <div class="form-group">
-                <label for="contact-nama">Nama Obat</label>
-                <input type="nama" name="nama" class="form-control" id="contact-nama" placeholder="Nama Obat" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-                <div class="validate"></div>
-              </div>
-              <div class="form-group">
-                <label for="contact-stok">Stok</label>
-                <input type="stok" name="stok" class="form-control" id="contact-stok" placeholder="Stok" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-                <div class="validate"></div>
-              </div>
-              <div class="form-group">
-                <label for="contact-harga">Harga</label>
-                <input type="harga" name="harga" class="form-control" id="contact-harga" placeholder="Harga" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-                <div class="validate"></div>
-              </div>
-              <br>
-            </form>
-          </div>
-          <br>
-          <br>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Data</title>
+</head>
+<body>
+  <h1>Data Pelanggan</h1>
+  <table border="1">
+    <tr>
+      <th>Nama</th>
+      <th>NoHP</th>
+      <th>Jumlah</th>
+    <tr>
+    <?php foreach($user as $u){ ?>
+      <td><?php echo $u->nama ?></td>
+      <td><?php echo $u->noHP ?></td>
+      <td><?php echo $u->jumlah ?></td>
+    </tr>
+    <?php } ?>
+    </table> 
+</body>
+</html>
